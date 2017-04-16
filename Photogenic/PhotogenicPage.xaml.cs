810f8870ec1080a083ec6e83817e3e0edb222fb7
Suman Ghosh <suman0989@gmail.com>
2017-04-16 20:40:37 +0530

    signup module

--- Photogenic/PhotogenicPage.xaml.cs
+++ Photogenic/PhotogenicPage.xaml.cs
@@ -1,12 +1,36 @@
 ﻿using Xamarin.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Photogenic
 {
 	public partial class PhotogenicPage : ContentPage
 	{
+
 		public PhotogenicPage()
 		{
 			InitializeComponent();
+		}
+
+		void signUpAction(object sender, System.EventArgs e)
+		{
+
+			/*
+ if (Regex.Match("myemail@validationexample.com", @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$").Success)
+     {
+                 //Valid email      
+     }else
+     {
+                //Not valid email      
+     }
+
+*/
+			UserModel user = new UserModel(fName.Text, lName.Text, email.Text, mobile.Text, pswd.Text);
+
+			DisplayAlert("SignUp Success", "Welcome \n" + user.fName +" "+ user.lName , "Ok");
 		}
 	}
 }
---
+++ Photogenic/UserModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Photogenic
+{
+	public class UserModel
+	{
+		public string fName { get; set; }
+public string lName { get; set; }
+public	string emailId { get; set; }
+public	string passWord { get; set; }
+public	string mobileNumber { get; set; }
+
+
+		public UserModel(string firstName, String lastName, String email, string mobileNumber, string pswd)
+		{
+			fName = firstName;
+			lName = lastName;
+			emailId = email;
+            passWord = pswd;
+
+		}
+	}
+}
